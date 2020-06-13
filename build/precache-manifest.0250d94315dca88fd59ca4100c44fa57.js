1da17fca60025f53ab05c8383e6dc0b76b0322f3
Sagar Chaudhari <sagarchaudhari250@gmail.com>
2020-06-14 01:02:13 +0530

    deplying build

---
+++ build/precache-manifest.0250d94315dca88fd59ca4100c44fa57.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c15349afbb03e5f3979365aedd521fa",
+    "url": "/todo-list/index.html"
+  },
+  {
+    "revision": "f00a94ed93a7f3e18629",
+    "url": "/todo-list/static/css/main.3fdd0536.chunk.css"
+  },
+  {
+    "revision": "9b1ab6d5d62a358bd0de",
+    "url": "/todo-list/static/js/2.17558ad2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-list/static/js/2.17558ad2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f00a94ed93a7f3e18629",
+    "url": "/todo-list/static/js/main.53db5da5.chunk.js"
+  },
+  {
+    "revision": "3f00e94e4fff06f6c6a8",
+    "url": "/todo-list/static/js/runtime-main.1ac559dd.js"
+  }
+]);
